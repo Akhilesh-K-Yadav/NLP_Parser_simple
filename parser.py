773813cf96f31590aaac5cd4284369ce9718fe41
Akhilesh-K-Yadav <akhilesh.sw.engineer@gmail.com>
2022-02-09 00:31:47 +0530

    Simple NLP Parser project by CS50AI

---
+++ parser.py
@@ -0,0 +1,100 @@
+import nltk
+import sys
+from nltk.tokenize import word_tokenize
+import re
+
+TERMINALS = """
+Adj -> "country" | "dreadful" | "enigmatical" | "little" | "moist" | "red"
+Adv -> "down" | "here" | "never"
+Conj -> "and" | "until"
+Det -> "a" | "an" | "his" | "my" | "the"
+N -> "armchair" | "companion" | "day" | "door" | "hand" | "he" | "himself"
+N -> "holmes" | "home" | "i" | "mess" | "paint" | "palm" | "pipe" | "she"
+N -> "smile" | "thursday" | "walk" | "we" | "word"
+P -> "at" | "before" | "in" | "of" | "on" | "to"
+V -> "arrived" | "came" | "chuckled" | "had" | "lit" | "said" | "sat"
+V -> "smiled" | "tell" | "were"
+"""
+
+NONTERMINALS = """
+S -> NP VP | S Conj VP | S Conj S
+AP -> Adj | AP
+PP -> P NP
+NP -> N | Det N | Det AP N | AP NP | NP Adv | NP PP
+VP -> V | VP NP | VP Adv | AP VP | VP Det | VP PP
+"""
+
+grammar = nltk.CFG.fromstring(NONTERMINALS + TERMINALS)
+parser = nltk.ChartParser(grammar)
+
+
+def main():
+
+    # If filename specified, read sentence from file
+    if len(sys.argv) == 2:
+        with open(sys.argv[1]) as f:
+            s = f.read()
+
+    # Otherwise, get sentence as input
+    else:
+        s = input("Sentence: ")
+
+    # Convert input into list of words
+    s = preprocess(s)
+
+    # Attempt to parse sentence
+    try:
+        trees = list(parser.parse(s))
+    except ValueError as e:
+        print(e)
+        return
+    if not trees:
+        print("Could not parse sentence.")
+        return
+
+    # Print each tree with noun phrase chunks
+    for tree in trees:
+        tree.pretty_print()
+        #tree.draw()
+
+        print("Noun Phrase Chunks")
+        for np in np_chunk(tree):
+            print(" ".join(np.flatten()))
+
+
+def preprocess(sentence):
+    """
+    Convert `sentence` to a list of its words.
+    Pre-process sentence by converting all characters to lowercase
+    and removing any word that does not contain at least one alphabetic
+    character.
+    """
+    #raise NotImplementedError
+    ret_words = []
+    words = word_tokenize(sentence.lower())
+    print(words)
+    for word in words:
+        if re.search("[a-z]", word) is None:
+            pass
+        else:
+            ret_words.append(word)
+    print(ret_words)
+    return ret_words
+
+
+def np_chunk(tree):
+    """
+    Return a list of all noun phrase chunks in the sentence tree.
+    A noun phrase chunk is defined as any subtree of the sentence
+    whose label is "NP" that does not itself contain any other
+    noun phrases as subtrees.
+    """
+    #raise NotImplementedError
+    print(tree.pos())
+    print(tree.treepositions('leaves'))
+    print(tree.leaf_treeposition(1))
+    print(tree.__getitem__(2))
+
+
+if __name__ == "__main__":
+    main()
